605695d4590b2af133f97058092a9b84835e26cb
wh-info <82549350+wh-info@users.noreply.github.com>
2023-01-23 23:58:26 +0100

    Add files via upload

---
+++ js/newVector2.js
@@ -0,0 +1,255 @@
+alwaysDraw={
+    element:undefined,
+    that:undefined,
+    flag:false
+   }
+   TraceArray=[]
+   currentTraces={}
+   var tt=true;;
+$(document).ready(function() {
+ 
+
+
+  function canvas() {
+	$("#canvas").attr({ 
+		"height": $("#pi").outerHeight(),
+		"width": $("#pi").outerWidth() 
+	});
+}
+	
+    Object.values(wormholes).forEach(wormhole => {
+
+        wormhole.dom = document.createElement('li');
+        wormholeel=$(wormhole.dom)
+
+        if(wormhole.showincol==0) wormholeel.appendTo('.items ul.type0')
+        if(wormhole.showincol==1) wormholeel.appendTo('.items ul.type1')
+        if(wormhole.showincol==2) wormholeel.appendTo('.items ul.type2')
+
+        wormholeel.text(wormhole.name).wrapInner($('<span />')).
+        mouseenter(function (){ viewTrace(wormhole, $(this)) }).
+        mouseleave(function (){ removeTrace(wormhole, $(this)) }).
+        click(function (){ drawTrace(wormhole,$(this)) })
+        if( wormhole.tooltip!="")
+        wormholeel.children("span").attr("class", "tooltip").attr("data-jbox-content", wormhole.tooltip.data_jbox_content);
+       ;
+
+       
+      });
+      $(".reset").click(function(){
+        savedTraces=[];
+        alwaysDraw.flag=false;
+        canvas();
+       
+        $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:"#1d4145"});;})
+        
+      })
+     makeColumn=(array, key)=>{
+        Object.values(array).forEach(el => {
+            el.dom = document.createElement('li');
+            this_el=$(el.dom)
+            this_el.appendTo('.items ul.'+key);
+            this_el.text(el.title).wrapInner($('<span />')).find("span").
+            mouseenter(function (){ viewFilterTrace(el, $(this), key) }).
+            mouseleave(function (){ removeFilterTrace(el, $(this)) }).
+            click(function (){ FilterTrace(el,$(this),key)})
+            if( el.tooltip!="")
+             this_el.children("span").attr("class", "tooltip").attr("data-jbox-content", el.tooltip.data_jbox_content);
+            ;
+          })
+     }
+     makeColumn(respawn, "respawn")
+     makeColumn(spawnIn, "spawnIn")
+     makeColumn(leadsTo, "leadsTo")
+     makeColumn(indMass, "indMass")
+    
+     var tool=new jBox('Mouse', {
+        attach: '.tooltip',
+        theme: 'TooltipDark',
+        getContent: 'data-jbox-content',
+    });
+    if(tt){
+        tool.attach($(".tooltip"));
+        $(".toggletooltip").css({backgroundColor:"#315970"})
+    }
+     else {
+       tool.detach($(".tooltip"));
+       $(".toggletooltip").css({backgroundColor:"#1e3644", boxSadow: "0px 0px 15px 30px rgb(0, 0, 0)"})
+     }
+    $(".toggletooltip").click(function(){
+
+        tt=!tt;
+     if(tt){
+         tool.attach($(".tooltip"));
+         $(this).css({backgroundColor:"#315970"})
+     }
+      else {
+        tool.detach($(".tooltip"));
+        $(this).css({backgroundColor:"#1e3644"})
+      }
+      })
+    
+      canvas();
+	$(window).resize(function() { // todo: get event, only repaint on mouse release
+		canvas();
+	});
+/*
+ respawn:["static"],
+        spawnIn:["class-2",],
+        leadsTo:["ls"], 
+        indMass:["up-to-battleship"], 
+        toMass:["2-000-000-000-kg"], 
+        lifeTime:["24h"],
+        */
+       
+        
+    function line(a,b) {
+       
+        $(a.dom).css({color:a.hover})
+        $(b.dom).css({color:b.hover})
+                 pad = 0;
+            apos=$(a.dom).children("span").position();
+            bpos= $(b.dom).children("span").position();
+            fx  = apos.left + 4;
+            fy  = apos.top +  $(a.dom).children("span").height()/2 + 1;
+            tx  = bpos.left + $(b.dom).children("span").width() - 2;
+            ty  =  bpos.top + $(b.dom).children("span").height()/2 + 1;
+            var cvs = document.getElementById('canvas');
+        
+            var ctx = cvs.getContext('2d');
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(tx,ty);
+            ctx.lineTo(fx,fy);
+            ctx.globalAlpha = 0.5;
+        
+            var gdt = ctx.createLinearGradient( tx,ty, fx,fy );
+            gdt.addColorStop(0, b.hover);
+            gdt.addColorStop(1, a.hover);
+        
+            ctx.strokeStyle = gdt;
+            ctx.stroke();
+        }
+    drawTrace=(element,that)=>{
+        savedTraces=[];
+        if(  alwaysDraw.flag==true  ){
+            alwaysDraw.flag=false;
+            $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:element.color});;})
+            return removeTrace(alwaysDraw.element,alwaysDraw.that)
+        }
+       alwaysDraw={
+        element:element,
+        that:that,
+        flag:true
+       }
+      
+       $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:" #06171c"});;})
+        removeFilterTrace(element, that)
+       viewTrace(alwaysDraw.element, alwaysDraw.that)
+       
+     }
+     function get(element,keys, array){
+        keys.forEach(el=>{
+            console.log(el)
+            Object.values(array).filter(e=>{return e.key==el}).forEach(array=>{ line(array, element)})
+        })
+       // return Object.values(array).filter(e=>{return e.key==key})
+     }
+    viewTrace=(element, that)=>{
+        canvas();
+        if(  alwaysDraw.flag==true  ) { 
+            element=alwaysDraw.element;
+            that=alwaysDraw.that
+        }
+        that.addClass('current');
+        get(element,element.respawn,respawn)
+        get(element,element.spawnIn,spawnIn)
+        get(element,element.leadsTo,leadsTo)
+        get(element,element.indMass,indMass)
+        //get(element,element.respawn,respawn).forEach(respawn=>{ line(respawn, element)})
+        //get(element.spawnIn,spawnIn).forEach(spawnIn=>{ line(spawnIn, element)})
+      //  get(element.leadsTo,leadsTo).forEach(leadsTo=>{ line(leadsTo, element)})
+       // get(element.indMass,indMass).forEach(indMass=>{ line(indMass, element)})
+       // get(element.toMass,toMass).forEach(toMass=>{ line(toMass, element)}) 
+      //  get(element.lifeTime,lifeTime).forEach(lifeTime=>{ line(lifeTime, element)}) 
+        if(savedTraces.length>0){
+            
+            currentTraces=Object.values(wormholes);
+            savedTraces.forEach(el=>currentTraces=currentTraces.filter(e=>{return e[el.key].indexOf(el.element.key)!=-1}))
+            savedTraces.forEach(el=>drawFilteresTraces(currentTraces, el.element))
+        }
+    }
+    removeTrace=(element, that)=>{
+        if(  alwaysDraw.flag==true  ) return
+        canvas();
+        that.removeClass('current');
+      //  $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:element.color});;})
+        removeFilterTrace(element, that)
+    }
+    savedTraces=[];
+    FilterTrace=(element, that, key)=>{
+        alwaysDraw.flag=false;
+       
+        id=savedTraces.map(e=>e.key).indexOf(key)
+       if(id>-1) savedTraces.splice(id,1);
+       else if(currentTraces.length>0){
+            savedTraces.push(
+             {
+                 key:key,
+                 element:element,
+                 that:that
+             }
+            )
+         }
+         if(savedTraces.length>0)viewFilterTrace(element, that, key)
+    }
+    drawFilteresTraces=(currentTraces, element)=>{
+        if(  alwaysDraw.flag==true  ) return
+        currentTraces.forEach(e=>{
+             line(element,e)
+        })
+    }
+    viewFilterTrace=(element, that, key)=>{
+        if(  alwaysDraw.flag==true  ) return
+        if(savedTraces.length>0) $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:" #06171c"});;})
+        
+       canvas();
+        if(savedTraces.length>0){
+            currentTraces=Object.values(wormholes);
+            savedTraces.forEach(el=>{
+                currentTraces=currentTraces.filter(e=>{return e[el.key].indexOf(el.element.key)!=-1})
+               
+            })
+            currentViewTraces=Object.values(currentTraces).filter(e=>{return e[key].indexOf(element.key)!=-1})
+            if(currentViewTraces.length>0) 
+                 {
+                currentTraces=currentViewTraces;
+                drawFilteresTraces(currentTraces, element)
+             }
+                 savedTraces.forEach(el=>{
+                drawFilteresTraces(currentTraces, el.element)
+                    })
+            
+        }
+        else
+        {
+            currentTraces=Object.values(wormholes).filter(e=>{return e[key].indexOf(element.key)!=-1})
+            drawFilteresTraces(currentTraces, element)
+            
+        }
+       
+    }
+   
+	removeFilterTrace=(element, that)=>{
+        if(  alwaysDraw.flag==true  ) return
+        canvas();
+        that.removeClass("color_selected");
+        $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:element.color});;})
+        if(savedTraces.length>0){
+            if(savedTraces.length>0) $('.items ').find("li").each(function(){$(this).removeClass('current').css({color:" #06171c"});;})
+            currentTraces=Object.values(wormholes);
+            savedTraces.forEach(el=>currentTraces=currentTraces.filter(e=>{return e[el.key].indexOf(el.element.key)!=-1}))
+            savedTraces.forEach(el=>drawFilteresTraces(currentTraces, el.element))
+        }
+    } 
+});
---
+++ js/setting2.js
@@ -0,0 +1,373 @@
+let tooltips={
+    0:{
+        data_jbox_content:"&#8594; Doesn't need to be scanned<br>&#8594; Visible as <i>Unidentified Wormhole</i>"
+    },
+    1:{
+        data_jbox_content:"&#8594; Can be <a>static</a> in C13 systems"
+    },
+    2:{
+        data_jbox_content:"Every <span class='ns'>Pochven</span> system is always connected to this type.<br>&#8594; <i>Ships allowed:</i> up to Bowhead<br>&#8594; <i>Excluded:</i> all other Freighters<br>&#8594; <i>Unique jump size mass:</i> 410 000 000 kg"
+    },
+    3:{
+        data_jbox_content:"&#8594; Is <a>static</a> only in one shattered <span class='C5'>C5</span> system; J011790"
+    },
+    4:{
+        data_jbox_content:"&#8594; Can be <a>static</a> in shattered <span class='C4'>C4</span> systems"
+    },
+    5:{
+        data_jbox_content:"&#8594; Can be <a>static</a> in shattered <span class='C4'>C4</span>, <span class='C5'>C5</span> and <span class='C6'>C6</span> systems"
+    },
+    6:{
+        data_jbox_content:"&#8594; Can be <a>static</a> in shattered <span class='C4'>C4</span> and <span class='C5'>C5</span> systems"
+    },
+    7:{
+        data_jbox_content:"Generic type designating an exit."
+    },
+    8:{
+        data_jbox_content:"&#8594; <i>Regions & constellations:</i> A-"
+    },
+    9:{
+        data_jbox_content:"&#8594; <i>Regions & constellations:</i> B-"
+    },
+    10:{
+        data_jbox_content:"&#8594; <i>Regions & constellations:</i> C-"
+    },
+    11:{
+        data_jbox_content:"&#8594; <i>Regions & constellations:</i> D-"
+    },
+    12:{
+        data_jbox_content:"&#8594; <i>Regions & constellations:</i> E-"
+    },
+    13:{
+        data_jbox_content:"&#8594; <i>Region & constellations:</i> F-"
+    },
+    14:{
+        data_jbox_content:"Unique shattered wormhole system, and only one with a name.<br>Has 4 NPC stations belonging to <i>Sisters of EVE</i>.<br>&#8594; <i>Region & constellation:</i> G-"
+    },
+    15:{
+        data_jbox_content:"25 shattered wormhole systems accessible only via 'frigate holes'.<br>&#8594; <i>System effect:</i> C6 Wolf Rayet<br>&#8594; <i>Anomalies:</i> C1-C3<br><i>&#8594; Jcode:</i> starts with 000<br>&#8594; <i>Region & constellations:</i> H-"
+    },
+    16:{
+        data_jbox_content:"<i>NullSec regions:</i><br>&#8594; Cobalt Edge, Etherium Reach, Kalevala Expanse,<br>&#8594; Malpais, Oasa, Outer Passage, Perrigen Falls, The Spire."
+    },
+    17:{
+        data_jbox_content:"5 unique shattered wormhole systems, each with its own class and system effect.<br>&#8594; <i>Region & constellations:</i> K-"
+    },
+    18:{
+        data_jbox_content:"&#8594; These structures are located across <i>known space</i>.<br>&#8594; Referred wormhole types spawn only in systems having them.<br>&#8594; First letter in each type reveals the name of a <i>Drifter</i> system destination."
+    },
+    19:{
+        data_jbox_content:"Bob knows why ..."
+    },
+    20:{
+        data_jbox_content:"It's type originated on the other side."
+    },
+    21:{
+        data_jbox_content:"&#8594; <i>Class:</i> C15<br><i>&#8594; Jcode:</i> J110145<br><i>&#8594; System effect:</i> C2 Cataclysmic Variable"
+    },
+    22:{
+        data_jbox_content:"&#8594; <i>Class:</i> C16<br><i>&#8594; Jcode:</i> J164710<br><i>&#8594; System effect:</i> C2 Magnetar"
+    },
+    23:{
+        data_jbox_content:"&#8594; <i>Class:</i> C17<br><i>&#8594; Jcode:</i> J200727<br><i>&#8594; System effect:</i> C2 Pulsar"
+    },
+    24:{
+        data_jbox_content:"&#8594; <i>Class:</i> C18<br><i>&#8594; Jcode:</i> J174618<br><i>&#8594; System effect:</i> C2 Wolf-Rayet"
+    },
+    25:{
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/Shole.webm'></video>&#8594; <i>In addition to:</i> Porpoise and HIC<br>&#8594; <i>Jump size mass:</i> 5 000 000 kg<br>&#8594; <i>Regen per hour:</i> 3 000 000 000 kg<br>&#8594; <span class='small'>Frigate holes (S)</span>; 00 in each type"
+    },
+    26:{
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/Mhole.webm'></video>&#8594; <i>In addition to:</i> DST and Nestor<br>&#8594; <i>Jump size mass:</i> 62 000 000 kg<br>&#8594; <span class='med'>Medium holes (M)</span>"
+    },
+    27:{
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/Lhole.webm'></video>&#8594; <i>In addition to:</i> Orca<br>&#8594; <i>Jump size mass:</i> 375 000 000 kg<br>&#8594; <span class='large'>Larger holes (L)</span>"
+    },
+    28:{
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/xlhole.webm'></video>&#8594; <i>Jump size mass:</i> 1 000 000 000 kg<br>&#8594; <span class='xlarge'>Very large holes (XL)</span>"
+    },
+    29:{
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/xlhole.webm'></video>&#8594; <i>Excluded:</i> Supercarrier and Titan<br>&#8594; <i>Jump size mass:</i> 2 000 000 000 kg<br>&#8594; <span class='xlarge'>Very large holes (XL)</span>"
+    },
+    30:{
+        data_jbox_content:"&#8594; <i>Class:</i> C14<br><i>&#8594; Jcode:</i> J055520<br><i>&#8594; System effect:</i> C2 Red Giant"
+    },
+    31:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c1.webm'></video>"
+    },
+    32:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c2.webm'></video>"
+    },
+    33:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c3.webm'></video>"
+    },
+    34:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c42.webm'></video>"
+    },
+    35:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c5.webm'></video>"
+    },
+    36:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c6.webm'></video>"
+    },
+    37:{
+        data_jbox_content:"<div class='tc'>Empire Space</div><br><span class='es'><video width='100' autoplay loop muted><source src='video/caldari.webm'></video><video width='100' autoplay loop muted><source src='video/amarr.webm'></video><br><video width='100' autoplay loop muted><source src='video/minmatar.webm'></video><video width='100' autoplay loop muted><source src='video/gallente.webm'></video></span>"
+    },
+    38:{
+        data_jbox_content:"Each NS region reflects its own nebula on a wormhole."
+    },
+    39:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/pochven.webm'></video>"
+    },
+    40:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/thera.webm'></video>"
+    },
+    41:{
+        data_jbox_content:"<video width='125' autoplay loop muted><source src='video/c13.webm'></video>"
+    },
+    42:{
+        data_jbox_content:"ccp when ?"
+    },
+    43:{
+        data_jbox_content:"Make sure to check visual cues and infotab for:<br>&#8594; wormhole size and destination<br>&#8594; wormhole mass and lifetime left"
+    },
+    44:{
+        data_jbox_content:"It's exit side is always present in destination system,<br>although it originates outside of it as wandering type.<br>&#8594; Known as <i>pseudo</i>, <i>reverse</i> or <i>K162 static</i>."
+    },
+    45:{
+        data_jbox_content:"Class 7"
+    },
+    46:{
+        data_jbox_content:"Class 8"
+    },
+    47:{
+        data_jbox_content:"Class 9"
+    },
+    48:{
+        data_jbox_content:"Class 25"
+    },
+    49:{
+        data_jbox_content:"&#8594; Leads to <span class='ls'>Turnur</span> system"
+    },
+}
+
+
+let respawn={
+    0:{   key:"safe",     hover:"#0dfa05", color:"#1d4145",  title:"SAFE", tooltip:tooltips[16]    },
+    1:{   key:"notsafe",     hover:"#e81e1e", color:"#1d4145",  title:"NOT SAFE", tooltip:""   },
+}
+
+let spawnIn={
+    0:{   key:"relicanalyser", hover:"#1f5eeb", color:"#1d4145",     title:"Relic Analyser",    tooltip:""},
+    1:{  key:"dataanalyser",  hover:"#36cccc", color:"#1d4145",     title:"Data Analyser",    tooltip:""},
+}
+
+let leadsTo={
+    0:{  key:"relic",      hover:"#79cef4", color:"#1d4145",         title:"Relic", tooltip:""    },
+    1:{  key:"data",      hover:"#79cef4", color:"#1d4145",         title:"Data",   tooltip:""  },
+    2:{  key:"drone",      hover:"#79cef4", color:"#1d4145",         title:"Drone",   tooltip:""  },
+    3:{  key:"ghost",      hover:"#79cef4", color:"#1d4145",         title:"Ghost",   tooltip:""  },
+    4:{  key:"sleeper",      hover:"#79cef4", color:"#1d4145",         title:"Sleeper Cache",   tooltip:""  },
+} 
+	
+let indMass={
+    0:{    key:"c1",    hover:"#42ffec", color:"#1d4145",  title:"C1", tooltip:""    },
+    1:{    key:"c2",    hover:"#42b3ff", color:"#1d4145",  title:"C2", tooltip:""   },
+    2:{   key:"c3",    hover:"#4265ff", color:"#1d4145",  title:"C3", tooltip:""   },
+    3:{    key:"c4",    hover:"#4230cf", color:"#1d4145",  title:"C4", tooltip:""  },
+    4:{    key:"c5",    hover:"#9c32ed", color:"#1d4145",  title:"C5", tooltip:""    },
+    5:{   key:"c6",     hover:"#f230dc", color:"#1d4145",  title:"C6", tooltip:""    },
+    6:{   key:"hs",     hover:"#0dfa05", color:"#1d4145",   title:"HS", tooltip:""   },
+    7:{   key:"ls",     hover:"#f0a800", color:"#1d4145",  title:"LS", tooltip:""   },
+    8:{   key:"ns",     hover:"#e81e1e", color:"#1d4145",  title:"NS", tooltip:""    },
+    9:{   key:"&nbsp;",    hover:"#ffffff", color:"#1d4145",  title:"", tooltip:""    },  
+    10:{   key:"c13",    hover:"#ffffff", color:"#1d4145",  title:"C13", tooltip:""   },
+    11:{   key:"droneregions",    hover:"#e81e1e", color:"#1d4145",  title:"Drone Regions", tooltip:tooltips[16]    },
+}
+let wormholes={
+    0:{
+        name:'Crumbling [pirate faction]', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["relicanalyser"],
+        leadsTo:["relic"], 
+        indMass:["hs"],
+    },
+    1:{
+        name:'Decayed [pirate faction]', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["relicanalyser"],
+        leadsTo:["relic"], 
+        indMass:["ls"],
+    },
+    2:{
+        name:'Ruined [pirate faction]', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["relicanalyser"],
+        leadsTo:["relic"], 
+        indMass:["ns","c1","c2","c3","c13"],
+    },
+    3:{
+        name:'Forgotten', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["relicanalyser"],
+        leadsTo:["relic","C1","C2","C3"], 
+        indMass:["c1","c2","c3","c4","c5","c6","c13"],
+    },
+    4:{
+        name:'', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:[""],
+        spawnIn:["",],
+        leadsTo:[""], 
+        indMass:[""], 
+    },
+    5:{
+        name:'Local [pirate faction]', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"], 
+        indMass:["hs"], 
+    },
+    6:{
+        name:'Regional [pirate faction]', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"], 
+        indMass:["ls"], 
+    },
+    7:{
+        name:'Central [pirate faction]', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"], 
+        indMass:["ns","c1","c2","c3","c13"],
+    },
+    8:{
+        name:'Unsecured', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"], 
+        indMass:["c1","c2","c3","c4","c5","c6","c13"],
+    },
+    9:{
+        name:'', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:[""],
+        spawnIn:["",],
+        leadsTo:[""], 
+        indMass:[""],
+    },
+    10:{
+        name:'AEGIS', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"],
+        indMass:["ls"], 
+    },
+    11:{
+        name:'SCC Secure Key Storage', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"],
+        indMass:["ls"], 
+    },
+    12:{
+        name:'[empire] Operation Center', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"],
+        indMass:["ls"], 
+    },
+    13:{
+        name:'', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:[""],
+        spawnIn:[""],
+        leadsTo:[""], 
+        indMass:[""], 
+    },
+    14:{
+        name:'Abandoned Research Complex', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser"],
+        leadsTo:["drone"], 
+        indMass:["droneregions"],
+    },
+    15:{
+        name:'', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:[""],
+        spawnIn:[""],
+        leadsTo:[""], 
+        indMass:[""], 
+        toMass:[""], 
+        lifeTime:[""],
+        sigLevel:[""],
+    },
+    16:{
+        name:'Lesser Covert Research Facility', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser"],
+        leadsTo:["ghost"], 
+        indMass:["hs"],
+    },
+    17:{
+        name:'Standard Covert Research Facility', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser"],
+        leadsTo:["ghost"], 
+        indMass:["ls"],
+    },
+    18:{
+        name:'Improved Covert Research Facility', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser"],
+        leadsTo:["ghost"], 
+        indMass:["ns"],
+    },
+    19:{
+        name:'Superior Covert Research Facility', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["dataanalyser"],
+        leadsTo:["ghost"], 
+        indMass:["c1","c2","c3","c4","c5","c6","c13"],
+    },
+    20:{
+        name:'', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:[""],
+        spawnIn:[""],
+        leadsTo:[""], 
+        indMass:[""], 
+    },
+    21:{
+        name:'Limited Sleeper Cache', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["relicanalyser","dataanalyser"],
+        leadsTo:["sleeper"], 
+        indMass:["hs","ls","ns"],
+    },
+    22:{
+        name:'Standard Sleeper Cache', hover:"yellow", color:"#1d4145",  tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["relicanalyser","dataanalyser"],
+        leadsTo:["sleeper"], 
+        indMass:["hs","ls","ns"],
+    },
+    23:{
+        name:'Superior Sleeper Cache', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["notsafe"],
+        spawnIn:["relicanalyser","dataanalyser"],
+        leadsTo:["sleeper"], 
+        indMass:["hs","ls","ns"],
+    },
+    24:{
+        name:'', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:[""],
+        spawnIn:[""],
+        leadsTo:[""], 
+        indMass:[""], 
+    },
+    25:{
+        name:'Silent Battleground', hover:"yellow", color:"#1d4145", tooltip:"", showincol:0,
+        respawn:["safe"],
+        spawnIn:["dataanalyser",],
+        leadsTo:["data"],
+        indMass:["c13"], 
+    },
+}
