eb1e4d46f4d12b9971f458d92638e12a3c760924
wh-info <82549350+wh-info@users.noreply.github.com>
2025-09-11 23:42:16 +0200

    Add files via upload

--- js/setting.js
+++ js/setting.js
@@ -81,13 +81,13 @@ let tooltips={
         data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/Mhole.webm'></video>&#8594; <i>In addition to:</i> DST and Nestor<br>&#8594; <i>Jump size mass:</i> 62 000 000 kg<br>&#8594; <span class='med'>Medium holes (M)</span>"
     },
     27:{
-        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/Lhole.webm'></video>&#8594; <i>In addition to:</i> Orca<br>&#8594; <i>Jump size mass:</i> 375 000 000 kg<br>&#8594; <span class='large'>Larger holes (L)</span>"
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/Lhole.webm'></video>&#8594; <i>In addition to:</i> Orca<br>&#8594; <i>Jump size mass:</i> 375 000 000 kg<br>&#8594; <span class='large'>Large holes (L)</span>"
     },
     28:{
-        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/xlhole.webm'></video>&#8594; <i>Jump size mass:</i> 1 000 000 000 kg<br>&#8594; <span class='xlarge'>Very large holes (XL)</span>"
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/xlhole.webm'></video>&#8594; <i>Jump size mass:</i> 1 000 000 000 kg<br>&#8594; <span class='xlarge'>Extra Large holes (XL)</span>"
     },
     29:{
-        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/xlhole.webm'></video>&#8594; <i>Excluded:</i> Supercarrier and Titan<br>&#8594; <i>Jump size mass:</i> 2 000 000 000 kg<br>&#8594; <span class='xlarge'>Very large holes (XL)</span>"
+        data_jbox_content:"<video class='vs' width='125' autoplay loop muted><source src='video/xlhole.webm'></video>&#8594; <i>Excluded:</i> Supercarrier and Titan<br>&#8594; <i>Jump size mass:</i> 2 000 000 000 kg<br>&#8594; <span class='xlarge'>Extra Large holes (XL)</span>"
     },
     30:{
         data_jbox_content:"&#8594; <i>Class:</i> C14<br><i>&#8594; Jcode:</i> J055520<br><i>&#8594; System effect:</i> C2 Red Giant"
